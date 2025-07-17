4ce58b24e4f0d46a722095032767e8ddf4508c28
Dipendra Singh <dipendras953@gmail.com>
2025-07-17 16:23:07 +0530

    Revert "Governance: add initialize realm"
    This reverts commit 0977fc6cb134c3f5236af6b07c760c43b8d0baf3.

--- programs/governance/src/events.rs
+++
@@ -1,7 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[event]
-pub struct RealmInitiated {
-    pub realm: Pubkey,
-    pub authority: Pubkey,
-}
--- programs/governance/src/instructions/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod initialize_realm;
-pub use initialize_realm::*;
